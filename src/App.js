0c30068f04a29d19b339b80602dc96237d049e84
jimmycababa <jimmycababa@gmail.com>
2021-06-29 23:21:48 -0500

    added primitive css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import TodoList from './TodoList';
+import './index.css'
 
 function App() {
   return (
     <div>
       <TodoList />
+      <input type="text"></input>
+      <button>Clear Completed</button>
+      <button>Delete Todos</button>
+      <div>0 Todos Remaining</div>
     </div>
   );
 }

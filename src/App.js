ebfd5501bafe5b1b516792ec4f0c1fa2814db8de
jimmycababa <jimmycababa@gmail.com>
2021-07-06 19:57:06 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Joke from './Joke';
 import './index.css'
 import Navbar from './Navbar';
+import MomJoke from './MomJoke';
 
 
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <Joke />
-      
+    <Router>
+      <div className="App">
+        <Navbar />
+          <Route path="/" exact component={Joke}/> 
+          <Route path="/MomJoke" exact component={MomJoke}/> 
     </div>
+
+
+    </Router>
+    
   );
 }
 

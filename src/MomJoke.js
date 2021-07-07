406049b40c229aa13a72019da87290013e4a60f1
jimmycababa <jimmycababa@gmail.com>
2021-07-07 13:59:13 -0500

    set up Mom page

--- src/MomJoke.js
+++ src/MomJoke.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function MomJoke() {
+    console.log('This component has been rendered');
+   
+
+      const [joke1, setJoke1] = useState('')
+      console.log("this component has been rendered")
+
+      useEffect( () => {
+          return axios.get('https://api.yomomma.info/',{headers: {Accept: 'application/json'}})
+          .then(res => setJoke1(res.data))
+          .catch(err => console.log(err))
+      }, [])
+
+        const fetchJoke1 = () => {
+            axios.get('https://api.yomomma.info/',{headers: {Accept: 'application/json'}})
+          .then(res => setJoke1(res.data))
+          .catch(err => console.log(err))
+        }
     return (
         <div>
-            <h1>Enter Mom Joke Here</h1>
+            <div className="card">
+  <h5 className="card-header">Mom Joke</h5>
+  <div className="card-body">
+    <p className="card-text">{joke1.joke1}</p>
+    <a href="#" className="btn btn-primary" onClick={fetchJoke1}>Get Joke</a>
+  </div>
+</div>
         </div>
     )
 }

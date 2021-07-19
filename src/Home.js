06948fee9eee61573e76e8f14c8795ffd79a75dc
jimmycababa <jimmycababa@gmail.com>
2021-07-19 10:54:20 -0500

    added form field with react hook

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function Home() {
+    const {register, handleSubmit} = useForm();
+    const onSubmit = (d) => alert(JSON.stringify(d))
+
+
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <label>
-                    Name:
-                    <input type="text" name="name" />
+                    First Name:
+                    <input {...register("FirstName")} />
                 </label>
-                <input type="submit" value="Submit" />
+                <label>
+                    Last Name:
+                    <input {...register("LastName")} />
+                </label>
+                <input type="submit" value="submit" />
             </form>
         </div>
     )

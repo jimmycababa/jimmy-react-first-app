3241f88e23bfdc85a8fcc3ca4482062571824d08
jimmycababa <jimmycababa@gmail.com>
2021-08-17 22:06:23 -0500

    added notes to navbar file

--- src/Navbar.js
+++ src/Navbar.js
@@ -40,3 +40,11 @@ export default function Navbar(props) {
         </div>
     )
 }
+
+// how do i want data to flow in the app
+// data in react is top down flow and unidirectional
+// option 1 is to handle the auth and database connection in the app component and wherever i need to use it i can pass down props and my component will use that info
+// option 2 React Context lets you keep a state separate from display componenets but brings that state into any comp where you need it
+// problem with option 1 means that the app is gonna be huge. prop drilling is the danger of doing it all in app
+// problem with using option 2 you will need to have context available
+// use auth-o for authorization
